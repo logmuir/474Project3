18eb53ba3f8e852c818381be4c954fb9a9e8501e
RJ Bowen <rjbowen97@gmail.com>
2018-11-06 23:22:29 -0500

    Made changes to the front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+import { ItineraryService } from './services/itinerary.service';
+import Itinerary from './models/itinerary.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  constructor(
+    //Private itineraryservice will be injected into the component by Angular Dependency Injector
+    private itineraryService: ItineraryService
+  ) { }
+
+  //Declaring the new itinerary Object and initilizing it
+  public newItinerary: Itinerary = new Itinerary()
+
+  //An Empty list for the visible itinerary list
+  itinerarysList: Itinerary[];
+
+
+  ngOnInit(): void {
+
+    //At component initialization the 
+    this.itineraryService.getItinerarys()
+      .subscribe(itinerarys => {
+        //assign the itinerarylist property to the proper http response
+        this.itinerarysList = itinerarys
+        console.log(itinerarys)
+      })
+  }
 }
