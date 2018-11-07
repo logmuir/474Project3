0de9c9fd6aec6fc8e368fd7c31a3bf51d30eb08f
RJ Bowen <rjbowen97@gmail.com>
2018-11-06 23:49:17 -0500

    Finished the front end for mongo!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -20,7 +20,55 @@ export class AppComponent implements OnInit {
 
   //An Empty list for the visible itinerary list
   itinerarysList: Itinerary[];
+  editItinerarys: Itinerary[] = [];
 
+
+  create() {
+    this.itineraryService.createItinerary(this.newItinerary)
+      .subscribe((res) => {
+        this.itinerarysList.push(res.data)
+        this.newItinerary = new Itinerary()
+      })
+  }
+
+  editItinerary(itinerary: Itinerary) {
+    console.log(itinerary)
+    if (this.itinerarysList.includes(itinerary)) {
+      if (!this.editItinerarys.includes(itinerary)) {
+        this.editItinerarys.push(itinerary)
+      } else {
+        this.editItinerarys.splice(this.editItinerarys.indexOf(itinerary), 1)
+        this.itineraryService.editItinerary(itinerary).subscribe(res => {
+          console.log('Update Succesful')
+        }, err => {
+          this.editItinerary(itinerary)
+          console.error('Update Unsuccesful')
+        })
+      }
+    }
+  }
+
+  doneItinerary(itinerary: Itinerary) {
+    itinerary.status = 'Done'
+    this.itineraryService.editItinerary(itinerary).subscribe(res => {
+      console.log('Update Succesful')
+    }, err => {
+      this.editItinerary(itinerary)
+      console.error('Update Unsuccesful')
+    })
+  }
+
+  submitItinerary(event, itinerary: Itinerary) {
+    if (event.keyCode == 13) {
+      this.editItinerary(itinerary)
+    }
+  }
+
+  deleteItinerary(itinerary: Itinerary) {
+    this.itineraryService.deleteItinerary(itinerary._id).subscribe(res => {
+      this.itinerarysList.splice(this.itinerarysList.indexOf(itinerary), 1);
+    })
+  }
 
   ngOnInit(): void {
 
