2cc165c290c4acc5a3635a3bc153e8c762bc2ece
bpham1012 <bpham@udel.edu>
2018-11-15 09:50:24 -0500

    Adding Angular Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { FormsModule } from '@angular/forms';
+import { CustomMaterialModule } from './core/material.module';
+// import { AppRoutingModule } from './core/app.routing.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ItineraryService } from './services/itinerary.service';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
+    // AppComponent,
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    CustomMaterialModule,
     FormsModule,
     NgbModule.forRoot()
   ],
