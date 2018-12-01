e375d735664243a1a504dca4e5593e17c917ea9d
RJ Bowen <rjbowen97@gmail.com>
2018-12-01 14:40:10 -0500

    Removed itinerary service lines from the app component:

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,8 +13,6 @@ export class AppComponent implements OnInit {
   title = 'Travel Express';
 
   constructor(
-    //Private itineraryservice will be injected into the component by Angular Dependency Injector
-    private itineraryService: ItineraryService
   ) { }
 
   //Declaring the new itinerary Object and initilizing it
@@ -26,60 +24,8 @@ export class AppComponent implements OnInit {
 
 
   create() {
-    this.itineraryService.createItinerary(this.newItinerary)
-      .subscribe((res) => {
-        this.itinerarysList.push(res.data)
-        this.newItinerary = new Itinerary()
-      })
   }
-
-  editItinerary(itinerary: Itinerary) {
-    console.log(itinerary)
-    if (this.itinerarysList.includes(itinerary)) {
-      if (!this.editItinerarys.includes(itinerary)) {
-        this.editItinerarys.push(itinerary)
-      } else {
-        this.editItinerarys.splice(this.editItinerarys.indexOf(itinerary), 1)
-        this.itineraryService.editItinerary(itinerary).subscribe(res => {
-          console.log('Update Succesful')
-        }, err => {
-          this.editItinerary(itinerary)
-          console.error('Update Unsuccesful')
-        })
-      }
-    }
-  }
-
-  doneItinerary(itinerary: Itinerary) {
-    itinerary.status = 'Done'
-    this.itineraryService.editItinerary(itinerary).subscribe(res => {
-      console.log('Update Succesful')
-    }, err => {
-      this.editItinerary(itinerary)
-      console.error('Update Unsuccesful')
-    })
-  }
-
-  submitItinerary(event, itinerary: Itinerary) {
-    if (event.keyCode == 13) {
-      this.editItinerary(itinerary)
-    }
-  }
-
-  deleteItinerary(itinerary: Itinerary) {
-    this.itineraryService.deleteItinerary(itinerary._id).subscribe(res => {
-      this.itinerarysList.splice(this.itinerarysList.indexOf(itinerary), 1);
-    })
-  }
-
+  
   ngOnInit(): void {
-
-    //At component initialization the 
-    this.itineraryService.getItinerarys()
-      .subscribe(itinerarys => {
-        //assign the itinerarylist property to the proper http response
-        this.itinerarysList = itinerarys
-        console.log(itinerarys)
-      })
   }
 }
